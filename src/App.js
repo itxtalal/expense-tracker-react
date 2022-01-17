8998aaa0dc1708f6a8f65f9cc42df766692a2e40
Talal <talalstudent@gmail.com>
2022-01-17 19:39:35 +0500

    Expense Form with Lifting the State Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -25,6 +26,10 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log(expense);
+  }
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -34,7 +39,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: e.target.value
+        // })
+        
+        // Updating multiple States through arrow function will gurantee
+        // the prevStates are the latest snapshots not an outdated State
+        // React schedules States updation and can update the State on
+        // the basis of an outdated State.
+
+
+        // If a newState depends on prevState
+        // Then always use arrow function
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredTitle: e.target.value};
+        // });
+    };
+
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: e.target.value
+        // })
+
+        // If a newState depends on prevState
+        // Then always use arrow function
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredAmount: e.target.value};
+        // });
+    };
+
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: e.target.value
+        // })
+
+        // If a newState depends on prevState
+        // Then always use arrow function
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredDate: e.target.value};
+        // });
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className="new-expense__control">
+                    <label htmlFor="title">Title</label>
+                    <input id='title' value={enteredTitle} type="text" onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="amount">Amount</label>
+                    <input id='amount' value={enteredAmount} type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="date">Date</label>
+                    <input id='date' value={enteredDate} type="date" min="2021-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    )
+};
+
+export default ExpenseForm;
