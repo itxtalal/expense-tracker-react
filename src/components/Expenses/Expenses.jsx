bd82f38adca6c2bcdaf9dc1f223f5c99a6e58f95
Talal <talalstudent@gmail.com>
2022-01-18 19:16:24 +0500

    Js to Jsx Files

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -7,7 +7,14 @@ import './Expenses.css';
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date} /> )
+      )}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -26,7 +33,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 }
